e18b80354aa77211dce1df0770abd64b277b953d
emmanuel-karanja <emmanuel.nganga.ke@gmail.com>
2026-01-02 17:52:39 +0300

    Updates

--- Arrays/MonotonicStack/MaxAreaHistogramStack.java
+++ Arrays/MonotonicStack/MaxAreaHistogramStack.java
@@ -25,14 +25,22 @@ public class MaxAreaHistogramStack {
 
         //we usually pad the end with a height of length 0, why?
         for(int i=0;i<=n;i++){
-            int h=(i==n)?0:heights[i]; 
+            int h=(i==n)?0:heights[i];   //set a sentinel height of 0
 
+            // The stack contains bars which are higher than the current height, we find the smaller or equal bar to the left
+            // since it represents the left edge of the current bounded rectangle( bounded by the current height)
+            // What this means is that we know for a given
+            // interval, the area of the histogram will be bounded by the shortest bar (which is the current)
+            // We find it's lower bound i.e. where we encounter a shorter or equal bar to the left. And since we'll have popped
+            // to that bar by 1, we need to let left be shortestIndex+1 (we had gone beyond we reign it back in) 
+            // We still calculate the areas of the bar between the current bar and the shorter or equal one to the left and find max
             
             while(!stack.isEmpty() && h < heights[stack.peek()]){
                 //calculate area
                 int height=heights[stack.pop()]; //height at the current top
                 int right=i; //current i is the right
 
+                // ie. 
                 int left= stack.isEmpty()? -1 : stack.peek(); //note this
 
                 //For each bar, find how far it can extend to the left and right before hitting a smaller bar.
